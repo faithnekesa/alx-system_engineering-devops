aa42bce28d069a6615cca511a5a6f583d8a49d7c
Faith <faynekesa@gmail.com>
2024-01-31 15:02:41 +0300

    Delete 0x0A-configuration_management directory
    Deferring to a later cohort

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# Script to install the puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,8 +0,0 @@
-# Using Puppet, create a manifest that kills a process named killmenow.
-
-exec { 'killmenow':
-  command  => 'pkill -f killmenow',
-  onlyif   => 'pgrep -f killmenow',
-  path     => '/usr/bin:/bin',
-  provider => shell,
-}
