c047ce3f3bfc572c64afba98dc79bb4c1027dcb4
faithnekesa <faynekesa@gmail.com>
2024-04-12 11:11:34 +0000

    pycodestyle correction

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -8,8 +8,9 @@ HEADERS = {'user-agent': 'my-app/0.0.1'}
 
 def count_words(subreddit, word_list, after="", word_dic={}):
     """
-    recursive function that queries the Reddit API, parses the title of all hot articles,
-    and prints a sorted count of given keywords (case-insensitive, delimited by spaces.
+    recursive function that queries the Reddit API, parses the title
+    of all hot articles,and prints a sorted count of given keywords 
+    (case-insensitive, delimited by spaces.
     Javascript should count as javascript, but java should not).
     """
     if not word_dic:
