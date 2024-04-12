d19a06eaee2e8c9dc6343d4af49500e6b0afd622
faithnekesa <faynekesa@gmail.com>
2024-04-12 11:07:31 +0000

    pycodestyle correction

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,8 +3,8 @@
 
 
 def recurse(subreddit, hot_list=[], count=0, after=None):
-    """recursive function that queries the Reddit API and returns a list containing the
-    titles of all hot articles for a given subreddit"""
+    """recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
