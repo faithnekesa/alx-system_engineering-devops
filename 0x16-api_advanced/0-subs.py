ba703ebf9694946604a9a6b0ec375ba3431b757c
faithnekesa <faynekesa@gmail.com>
2024-04-12 10:59:55 +0000

    Advanced api tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,12 @@
 def number_of_subscribers(subreddit):
     """Function that queries reddit API and returns number of subs to subreddit"""
     import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
